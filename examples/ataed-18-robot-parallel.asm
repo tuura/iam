6ede03b0a2aab70cab226e354dbc318f88f55f67
Georgy Lukyanov <georgiylukjanov@gmail.com>
2018-06-10 15:19:13 +0100

    Add ATAED'18 example source code.

---
+++ examples/ataed-18-robot-parallel.asm
@@ -0,0 +1,7 @@
+Load R0 254
+AdjustVelocity R0 0
+
+Load R1 255
+AdjustVelocity R1 1
+CheckOperationStatus R2 0 1
+
---
+++ examples/ataed-18-robot-sequential.asm
@@ -0,0 +1,5 @@
+Load R0 254
+AdjustVelocity R0 0
+AdjustVelocity R0 1
+CheckOperationStatus R2 0 1
+
