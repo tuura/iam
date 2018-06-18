58c578af37ae0aa3f78e2a3dcfff71777d00cd95
Georgy Lukyanov <georgiylukjanov@gmail.com>
2018-06-18 12:06:12 +0100

    implement 'jumpZero' semantics with Selective.

--- src/Machine/Semantics.hs
+++ src/Machine/Semantics.hs
@@ -5,12 +5,14 @@
 
 module Machine.Semantics where
 
-import Prelude hiding (read)
+import Prelude hiding (Monad, read)
+import qualified Prelude (Monad)
 import Control.Monad (join)
 import Metalanguage
 import Machine.Types
 import Machine.Instruction
 import Data.List.NonEmpty
+import Control.Selective
 
 -- | 'MachineKey' will instantiate the 'k' type variable in the 'Semantics'
 --   metalanguage.
@@ -21,6 +23,8 @@ data MachineKey = Reg  Register
                 | IR
                 | Prog InstructionAddress
     deriving (Show, Eq, Ord)
+
+type Monad m = (Selective m, Prelude.Monad m)
 
 -- | Functorial semantics is data independent and may have a most one
 --   static dependency. May be used for
@@ -49,9 +53,13 @@ semanticsA (Set reg simm)    = setA reg simm
 semanticsA (Store reg addr)  = store reg addr
 semanticsA (Add reg addr)    = add reg addr
 semanticsA (Jump simm)       = jump simm
-semanticsA (JumpZero _)      = const (const Nothing)
 semanticsA (AdjustVelocity reg addr)  = adjust reg addr
 semanticsA (CheckOperationStatus reg addr1 addr2) = statusCheck reg addr1 addr2
+
+semanticsS :: Instruction -> Semantics Selective MachineKey Value ()
+semanticsS (JumpZero simm)   = jumpZero simm
+semanticsS (LoadMI _ _)      = const (const Nothing)
+semantics i                  = semanticsA i
 
 -- | Monadic semantics may involve data dynamic analysis and must be executed
 --   on a concrete machine state.
@@ -60,8 +68,7 @@ semanticsA (CheckOperationStatus reg addr1 addr2) = statusCheck reg addr1 addr2
 --   instruction may be only assigned monadic semantics.
 semanticsM :: Instruction -> Semantics Monad MachineKey Value ()
 semanticsM (LoadMI reg addr) = loadMI reg addr
-semanticsM (JumpZero simm)   = jumpZero simm
-semanticsM i                 = semanticsA i
+semanticsM i                 = semanticsS i
 
 -- | Halt the execution.
 --   Functor.
@@ -136,15 +143,19 @@ loadMI reg addr read write = Just $ do
     write (Reg reg) (read (Addr addr'))
 
 -- | Jump if 'Zero' flag is set.
---   Monadic.
-jumpZero :: SImm8 -> Semantics Monad MachineKey Value ()
-jumpZero simm read write = Just $ do
-    zero <- read (F Zero)
-    if (zero == 1) then
-        write IC ((+) <$> read IC <*> pure simm)
-    else pure ()
+--   Selective.
+jumpZero :: SImm8 -> Semantics Selective MachineKey Value ()
+jumpZero simm read write = Just $
+    ifS ((==) <$> read (F Zero) <*> pure 1)
+        (write IC ((+) <$> read IC <*> pure simm))
+        (pure ())
 
 blockSemanticsA :: [Instruction] -> Semantics Applicative MachineKey Value ()
 blockSemanticsA xs = \read write->
     foldr (\x acc -> ((*>)) <$> acc <*> semanticsA x read write) nop xs
+    where nop = Just $ pure ()
+
+blockSemanticsS :: [Instruction] -> Semantics Selective MachineKey Value ()
+blockSemanticsS xs = \read write->
+    foldr (\x acc -> ((*>)) <$> acc <*> semanticsS x read write) nop xs
     where nop = Just $ pure ()
