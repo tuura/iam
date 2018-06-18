3d8c03ec3c6af358467863b2fc26f818bc03cf0f
Georgy Lukyanov <georgiylukjanov@gmail.com>
2018-06-18 12:06:12 +0100

    implement 'jumpZero' semantics with Selective.

--- src/Machine/Semantics/Simulate.hs
+++ src/Machine/Semantics/Simulate.hs
@@ -12,37 +12,37 @@ import Metalanguage
 import Machine.State
 import Machine.Semantics
 
--- | Execute the semantics in a stateful context to get the result and the
---   final state of the microarchitecture.
-simulate :: Semantics Monad k v a
-         -> s
-         -> (k -> State s v)
-         -> (k -> State s v -> State s ())
-         -> Maybe (a, s)
-simulate task s0 read write =
-    (flip runState) s0 <$> (task read write)
--- Example:
--- simulate (mconcat $ map semantics $ map snd ex1) defaultState readState writeState
+-- -- | Execute the semantics in a stateful context to get the result and the
+-- --   final state of the microarchitecture.
+-- simulate :: Semantics Monad k v a
+--          -> s
+--          -> (k -> State s v)
+--          -> (k -> State s v -> State s ())
+--          -> Maybe (a, s)
+-- simulate task s0 read write =
+--     (flip runState) s0 <$> (task read write)
+-- -- Example:
+-- -- simulate (mconcat $ map semantics $ map snd ex1) defaultState readState writeState
 
---------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------
 
-readState :: MachineKey -> State MachineState Value
-readState k = do
-    s <- get
-    case k of Reg  reg  -> pure $ (Map.!) (registers s) reg
-              Addr addr -> pure $ (Map.!) (memory s)    addr
+-- readState :: MachineKey -> State MachineState Value
+-- readState k = do
+--     s <- get
+--     case k of Reg  reg  -> pure $ (Map.!) (registers s) reg
+--               Addr addr -> pure $ (Map.!) (memory s)    addr
 
-writeState :: MachineKey -> Value -> State MachineState ()
-writeState k v = do
-    s <- get
-    case k of Reg reg   -> let rs' = Map.alter (const . Just $ v) reg (registers s)
-                           in put $ s {registers = rs'}
-              Addr addr -> let mem' = Map.alter (const . Just $ v) addr (memory s)
-                           in put $ s {memory = mem'}
+-- writeState :: MachineKey -> Value -> State MachineState ()
+-- writeState k v = do
+--     s <- get
+--     case k of Reg reg   -> let rs' = Map.alter (const . Just $ v) reg (registers s)
+--                            in put $ s {registers = rs'}
+--               Addr addr -> let mem' = Map.alter (const . Just $ v) addr (memory s)
+--                            in put $ s {memory = mem'}
 
-readIO :: (Show k, Read v) => k -> IO v
-readIO k = do putStr (show k ++ " = "); Prelude.read <$> getLine
+-- readIO :: (Show k, Read v) => k -> IO v
+-- readIO k = do putStr (show k ++ " = "); Prelude.read <$> getLine
 
-writeIO :: (Show k, Show v) => k -> v -> IO ()
-writeIO k v = putStrLn (show k ++ " := " ++ show v);
---------------------------------------------------------------------------------
+-- writeIO :: (Show k, Show v) => k -> v -> IO ()
+-- writeIO k v = putStrLn (show k ++ " := " ++ show v);
+-- --------------------------------------------------------------------------------
