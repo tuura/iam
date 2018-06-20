d6d54d26e0eb212224673a27939a473df410b8de
Georgy Lukyanov <georgiylukjanov@gmail.com>
2018-06-20 18:51:14 +0100

    Remove obsolete modules.

--- src/Machine/Examples/Common.hs
+++ src/Machine/Examples/Common.hs
@@ -5,7 +5,6 @@ module Machine.Examples.Common where
 
 import qualified Data.Map as Map
 import Machine.Types
-import Machine.State
 import Machine.Instruction
 import Metalanguage
 import Machine.Semantics
@@ -19,22 +18,6 @@ import Control.Applicative
 import Data.List.NonEmpty hiding (zip)
 import Text.Read (read)
 import System.Process (callCommand)
-
-emptyRegisters :: RegisterBank
-emptyRegisters = Map.fromList [(R0, 0), (R1, 0), (R2, 0), (R3, 0)]
-
-emptyFlags :: Flags
-emptyFlags = Map.fromList [(Zero, 0), (Halted, 0)]
-
-templateState :: Memory -> MachineState
-templateState mem = MachineState { registers = emptyRegisters
-                                 , instructionCounter = 0
-                                 , instructionRegister = Jump 0
-                                 , program = []
-                                 , flags = emptyFlags
-                                 , memory = mem
-                                 , clock = 0
-                                 }
 
 ex1 :: Program -- [(InstructionAddress, Instruction)]
 ex1 = zip [0..]
--- src/Machine/Examples/InstructionDeps.hs
+++
@@ -1,8 +0,0 @@
-module Machine.Examples.InstructionDeps where
-
-import Machine.Types
-import Machine.State
-import Machine.Instruction
-import Machine.Semantics.Dependencies
-import Data.Maybe (fromJust)
-
