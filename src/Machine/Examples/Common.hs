ee8468f5444e78befac159dd6d44597d1a01767e
Georgy Lukyanov <georgiylukjanov@gmail.com>
2018-06-20 19:01:28 +0100

    Add the good old array summation example.

--- src/Machine/Examples/Common.hs
+++ src/Machine/Examples/Common.hs
@@ -1,76 +1,41 @@
-{-# LANGUAGE ConstraintKinds,
-             RankNTypes #-}
-
 module Machine.Examples.Common where
 
-import qualified Data.Map as Map
-import Machine.Types
-import Machine.Instruction
-import Metalanguage
-import Machine.Semantics
-import Machine.Semantics.Dependencies
-import Machine.Semantics.Oracle
-import Machine.Semantics.Graph.Dataflow
-import Data.Maybe (fromJust)
-import Algebra.Graph
-import Data.Functor.Const
-import Control.Applicative
-import Data.List.NonEmpty hiding (zip)
-import Text.Read (read)
-import System.Process (callCommand)
+import Data.SBV
+import Machine.Semantics.Symbolic
+import Machine.Semantics.Symbolic.Types
+import Machine.Semantics.Symbolic.Instruction
+import Machine.Semantics.Symbolic.State
+import Machine.Semantics.Symbolic.Machine
+import Machine.Assembly
 
-ex1 :: Program -- [(InstructionAddress, Instruction)]
-ex1 = zip [0..]
-    [ Load R0 0
-    , Add  R0 1
-    -- , JumpZero 1
-    -- , Add  R0 1
-    ]
+emptyRegisters :: RegisterBank
+emptyRegisters = mkSFunArray $ const 0
 
-writeSvgProgramDataGraph :: FilePath -> FilePath -> IO ()
-writeSvgProgramDataGraph sourceCode svgFileName = do
-    src <- readProgram sourceCode
-    let graph = fromJust . programDataGraph $ src
-        dotFileName = Prelude.takeWhile (/= '.') svgFileName ++ ".dot"
-    writeFile dotFileName (drawGraph graph)
-    callCommand ("dot -Tsvg " ++ dotFileName ++ " -o " ++ svgFileName)
+emptyFlags :: Flags
+emptyFlags = mkSFunArray $ const 0
 
---------------------------------------------------------------------------------
--- ATAED'18 paper examples -----------------------------------------------------
---------------------------------------------------------------------------------
+initialiseMemory :: [(SBV MemoryAddress, SBV Value)] -> Memory
+initialiseMemory =
+    foldr (\(a, v) m -> writeArray m a v) (mkSFunArray $ const 0)
 
-infixl 4 *+>
+dumpMemory :: Value -> Value -> Memory -> [SBV Value]
+dumpMemory from to m = map (readArray m) [literal from..literal to]
 
-(*+>) :: Semantics Applicative k v a
-      -> Semantics Applicative k v b
-      -> Semantics Applicative k v b
-(*+>) = (liftA2 (*>))
+templateState :: Script -> Memory -> MachineState
+templateState src mem = MachineState { registers = emptyRegisters
+                               , instructionCounter = 0
+                               , instructionRegister = literal $ Jump 0
+                               , program = assemble src
+                               , flags = emptyFlags
+                               , memory = mem
+                               , clock = 0
+                               }
 
+prover = z3 { verbose = True
+            , redirectVerbose = Just "example.smt2"
+            , timing = PrintTiming
+            , printBase = 10
+            }
 
-loadGraph = fromJust $ instructionGraph (0, (Load R0 0))
-jumpGraph = fromJust $ instructionGraph (0, (Jump 42))
-addGraph = fromJust $ instructionGraph (0, (Add R1 1))
-wg = writeFile "ataed-2018-paper/img/loadJumpAdd.dot"
-     (drawGraph $ overlays [loadGraph, jumpGraph, addGraph])
-
-
-oracle1 = concurrencyOracle (semanticsA (Load R0 0)) (semanticsA (Load R1 2))
-
-oracle2p1 = [Load R0 0, Add R0 1]
-oracle2p2 = [Load R1 1]
-
-oracle2 = concurrencyOracle (blockSemanticsA oracle2p1)
-                            (blockSemanticsA oracle2p2)
-
-oracle3p1 = [Load R0 0, Add R0 1, Jump 1]
-oracle3p2 = [Load R1 1, Add R1 2, Jump 1]
-
-oracle3 = concurrencyOracle (blockSemanticsA oracle3p1)
-                            (blockSemanticsA oracle3p2)
-
-tr  k    = Const [Left k]
-tw k fv = Const [Right k] *> fv *> pure ()
-
--- ghci> load R0 0 tr tw *> load R1 1 tr tw
--- 
--- (fromJust $ load R0 0 tr tw) *> (fromJust $ load R1 1 tr tw)
+steps :: Int
+steps = 100
---
+++ src/Machine/Examples/SumArray.hs
@@ -0,0 +1,58 @@
+module Machine.Examples.SumArray where
+
+import Prelude hiding (subtract)
+import Text.Pretty.Simple (pPrint)
+import Data.SBV hiding (label)
+import Machine.Semantics.Symbolic
+import Machine.Semantics.Symbolic.Machine
+import Machine.Semantics.Symbolic.Types
+import Machine.Semantics.Symbolic.Instruction
+import Machine.Semantics.Symbolic.State
+import Machine.Assembly
+-- import Machine.Semantics (readRegister)
+-- import Machine.Examples.Common
+
+prover = z3 { verbose = True
+            , redirectVerbose = Just "example.smt2"
+            , timing = PrintTiming
+            , printBase = 10
+            }
+
+initialiseMemory :: [(SBV MemoryAddress, SBV Value)] -> Memory
+initialiseMemory =
+    foldr (\(a, v) m -> writeArray m a v) (mkSFunArray $ const 0)
+
+runModel :: Int -> MachineState -> MachineState
+runModel steps state
+    | steps == 0 = state
+    | otherwise  = ite halted state (runModel (steps - 1) nextState)
+  where
+    halted    = (./= 0) $ readArray (flags state) (literal Halted)
+    nextState = snd $ run executeInstruction state
+--------------------------------------------------------------------------------
+sumArray :: Script
+sumArray = do
+    load R0 0       -- sum := 0, sum accumulator
+    load R2 254     -- i := n,   loop counter
+    loop <- label
+    store R2 255
+    loadMI R1 255   -- load a[i]
+    store R1 254    -- put a[i] to cell 254
+    add R0 254      -- sum := sum + a[i]
+    add R2 253      -- i := i - 1
+    jumpZero 1
+    goto loop
+    halt
+
+theoremSumArray :: Int -> IO ThmResult
+theoremSumArray n = proveWith prover $ do
+    summands <- mkForallVars n
+    let memory = initialiseMemory $  [(0, 0)]
+                                  ++ zip [1..] summands
+                                  ++ [ (253, -1)
+                                     , (254, fromIntegral n)
+                                     ]
+        steps = 10000
+        finalState = runModel steps $ templateState (assemble sumArray) memory
+        result = readArray (registers finalState) (literal R0)
+    pure $ result .== sum summands &&& clock finalState .< 10000
