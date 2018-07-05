88ec06bc5622b1f55ae728fe4ecc1e7db0227e3c
Georgy Lukyanov <georgiylukjanov@gmail.com>
2018-07-05 18:50:08 +0100

    Purge the obsolete Free-AST.

--- src/Machine/Semantics/Symbolic.hs
+++ src/Machine/Semantics/Symbolic.hs
@@ -15,12 +15,8 @@ module Machine.Semantics.Symbolic where
 import Data.SBV
 import Data.Maybe (fromJust)
 import Control.Monad.State (get, modify)
-import Control.Selective (handle)
-import Metalanguage
-import AST
 import Machine.Semantics
 import Machine.Instruction
-import Machine.Assembly
 import Machine.Semantics.Symbolic.Types
 import Machine.Semantics.Symbolic.State
 import Machine.Semantics.Symbolic.Machine
@@ -29,23 +25,23 @@ import Machine.Value (toValue, fromValue)
 import Machine.Instruction.Encode
 import Machine.Instruction.Decode
 
-buildAST :: Semantics Machine.Semantics.Monad (MachineKey r addr iaddr flag) v a
-         -> Maybe (AST ( MachineKey r addr iaddr flag) v a)
-buildAST computation = computation Read Write
+-- | Run symbolic execution using the provided initial state and
+--   the maximum number of state transitions
+runModel :: Int -> MachineState -> MachineState
+runModel steps state
+    | steps == 0 = state
+    | otherwise  = ite halted state (runModel (steps - 1) nextState)
+  where
+    halted    = (./= 0) $ readArray (flags state) (literal Halted)
+    nextState =
+        case executeInstruction readKey writeKey of
+            Just computation -> snd $ run computation state
+            Nothing -> error
+                "Machine.Semantics.Symbolic.runModel: no semantics for the next instruction."
 
-interpretSymbolic :: AST (MachineKey (SBV Register) (SBV MemoryAddress)
-                                     (SBV InstructionAddress) (SBV Flag)) (SBV Value) a
-                  -> Machine a
-interpretSymbolic = \case
-    Read  k               -> readKey k
-    Write k v             -> writeKey k (interpretSymbolic v)
-    Fmap  func fa         -> fmap func (interpretSymbolic fa)
-    Pure  x               -> pure x
-    Star  ffunc fa        -> (interpretSymbolic ffunc) <*> (interpretSymbolic fa)
-    Handle choice handler -> handle (interpretSymbolic choice) (interpretSymbolic handler)
-    Bind  a f             -> (interpretSymbolic a) >>= (\x -> interpretSymbolic (f x))
-
-readKey :: MachineKey (SBV Register) (SBV MemoryAddress) (SBV InstructionAddress) (SBV Flag) -> Machine (SBV Value)
+-- | Instance of the Machine.Metalanguage read command for symbolic execution
+readKey :: MachineKey (SBV Register) (SBV MemoryAddress) (SBV InstructionAddress) (SBV Flag)
+        -> Machine (SBV Value)
 readKey = \case
     Reg  reg  -> readRegister reg
     Addr addr -> readMemory   addr
@@ -54,8 +50,10 @@ readKey = \case
     IR        -> readInstructionRegister
     Prog addr -> readProgram addr
 
+-- | Instance of the Machine.Metalanguage write command for symbolic execution
 writeKey :: MachineKey (SBV Register) (SBV MemoryAddress) (SBV InstructionAddress) (SBV Flag)
-         -> Machine (SBV Value) -> Machine ()
+         -> Machine (SBV Value)
+         -> Machine ()
 writeKey k v = case k of
     Reg  reg  -> v >>= writeRegister reg
     Addr addr -> v >>= writeMemory   addr
@@ -66,18 +64,9 @@ writeKey k v = case k of
     IR        -> v >>= writeInstructionRegister
     Prog addr -> error "Machine.Semantics.Symbolic: Can't write Program"
 
+-- | Assemble a list of instructions into a symbolic execution-ready program
 assemble :: [Instruction Register MemoryAddress Flag Byte] -> Program
 assemble s = foldr (\(c, p) a -> writeArray a p c) a0 (zip (map literal prg) [0..])
   where
     a0  = mkSFunArray (const $ literal 0)
     prg = map encode $ s
-
-runModel :: Int -> MachineState -> MachineState
-runModel steps state
-    | steps == 0 = state
-    | otherwise  = ite halted (runModel 0 state) (runModel (steps - 1) nextState)
-  where
-    halted    = (./= 0) $ readArray (flags state) (literal Halted)
-    nextState =
-        let ast = fromJust $ buildAST executeInstruction
-        in snd $ run (interpretSymbolic ast) state
